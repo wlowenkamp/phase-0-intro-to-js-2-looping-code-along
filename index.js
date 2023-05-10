2f7f471e2074429037094ba33e251035a7477894
Will Lowenkamp <lowenkamp97@gmail.com>
2023-05-10 08:50:43 -0400

    changes for assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(name, event){
+    let card= [];
+    for (let i=0; i < name.length; i++) {
+        card.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    } 
+    return card;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "birthday"));
+
+function countDown(num) {
+    while (num>= 0) {
+        console.log(num--);
+    }
+}
+countDown(10);
